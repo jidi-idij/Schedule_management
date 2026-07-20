ce35a470e5ed0f76355bdd21a08fee4dae487665
jidi_idij <2952130105@qq.com>
2026-07-20 12:31:13 +0800

    fix: 使用 HashRouter 修复 GitHub Pages 空白页

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
+import { HashRouter } from 'react-router'  // 改为 HashRouter
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>  {/* 不需要 basename */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
